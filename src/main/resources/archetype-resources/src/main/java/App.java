86848ebfa7fa661303ce73ca94579fca55d6d68a
Lambonik <nikita1994g@mail.ru>
2023-03-01 16:38:06 +0300

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.netology.Project5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
